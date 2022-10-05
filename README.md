18307a748e6b16256673442e643962363c0fc0a3
Piggey <pepcio3320@gmail.com>
2022-10-05 20:04:46 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Programming in Python
+
+
+## Excercise 1: Chase - wolf hunting sheep simulation
+
+
+### TODO:
+- [ ] create an installable python package with `distutils` and `setuptools`
+- [ ] argument parsing
+- [ ] logging with many levels
+- [ ] exporting json and csv simulation data
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='chase',
+    version='0.0.1',
+    description='Programming in Python Excercise 1',
+    url='https://github.com/Piggey/programming-in-python',
+    author='Dawid Sośnia',
+    author_email='236653@edu.p.lodz.pl',
+    license='GPL-3.0 license',
+    packages=['chase'],
+    # install_requires=['mpi4py>=2.0',
+    #                   'numpy',                     
+    #                   ],
+
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.10',
+    ]
+)
