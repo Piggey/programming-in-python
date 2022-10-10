f5ea516d31d21a0972f54ac833e8a5c82f9f15af
Piggey <pepcio3320@gmail.com>
2022-10-10 17:47:24 +0200

    constructor for `Wolf` and `Sheep` classes

--- chase/Sheep.py
+++ chase/Sheep.py
@@ -1,3 +1,8 @@
 
-class Sheep():
-    pass
+class Sheep:
+
+    def __init__(self, sheep_id: int, pos_x: float, pos_y: float, move_dist: float):
+        self.sheep_id = sheep_id
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.move_dist = move_dist
--- chase/Wolf.py
+++ chase/Wolf.py
@@ -1,3 +1,7 @@
 
-class Wolf():
-    pass
+class Wolf:
+    
+    def __init__(self, pos_x: float, pos_y: float, move_dist: float):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.move_dist = move_dist
